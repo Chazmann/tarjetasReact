61308a093e1b2fc98815ea7779a0bda63d49b572
Leandro Chazarreta <lchaz85@gmail.com>
2023-08-23 00:01:55 -0300

    Actualizado iconos y nombre repo

---
+++ src/FirstApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const FirstApp = ( {tiTle, subTitle} ) => {
+    return (<>
+        <h1>{tiTle}</h1>
+        <h3>{subTitle}</h3>
+
+    </>
+
+    )
+}
---
+++ src/portalEventos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeaderNav from './componentes/HeaderNav'
+import CardContainer from './componentes/CardContainer'
+import Foo from './componentes/Foo'
+
+const PortalEventos = () => {
+  return (
+    <>
+    
+      <CardContainer />
+      
+    </>
+  )
+}
+
+export default PortalEventos
